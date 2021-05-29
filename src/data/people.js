904c2aea621dc1f06cd185eecc5392d71eda5762
joaowillamy <willamyse@gmail.com>
2021-05-29 10:37:45 -0300

    feat(new-data): add some new people

--- src/data/people.js
+++ src/data/people.js
@@ -375,4 +375,274 @@ export const people = {
         familyType: type.aunt,
         bingo: [],
     },
+    "Camila-m": {
+        name:"Camila",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "Brendon-m": {
+        name:"Brendon",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "shirley-m": {
+        name:"Shirley",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "isabel-m": {
+        name:"Isabel",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "cassio-m": {
+        name:"Cassio",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "mateus-m": {
+        name:"Mateus",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "junior-m": {
+        name:"Junior",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "neto-m": {
+        name:"Neto",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "chris-m": {
+        name:"Chris",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "kamila-m": {
+        name:"Kamila",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "orle-m": {
+        name:"Orle",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "yago-m": {
+        name:"Yago",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "amanda-m": {
+        name:"Amanda",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "rylderson-m": {
+        name:"Rylderson",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "luiza-m": {
+        name:"Luiza",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "dayane-m": {
+        name:"Dayane",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "nadia-m": {
+        name:"Nadia",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "ellen-m": {
+        name:"Ellen",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "milleny-m": {
+        name:"Milleny",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "caina-m": {
+        name:"Caina",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "anderson-j": {
+        name:"Anderson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "josivan-j": {
+        name:"Josivan",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jackson-j": {
+        name:"Jackson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jeferson-j": {
+        name:"Jeferson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "dhonas-j": {
+        name:"Dhonas",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "marcelo-j": {
+        name:"Marcelo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "eric-j": {
+        name:"Eric",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "bruno-j": {
+        name:"Bruno",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "davi-j": {
+        name:"Davi",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "nelio-j": {
+        name:"Nelio",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "wesley-j": {
+        name:"Wesley",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jayara-j": {
+        name:"Jayara",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "mike-j": {
+        name:"Mike",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "carmen-j": {
+        name:"Carmen",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "ricardo-j": {
+        name:"Ricardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "fabio-j": {
+        name:"Fabio",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "junior-leo-j": {
+        name:"Junior",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "pedro-j": {
+        name:"Pedro",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "matheus-j": {
+        name:"Matheus",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jaine-j": {
+        name:"Jaine",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "igor-j": {
+        name:"Igor",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "vinicius-j": {
+        name:"Vinicius",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "iuri-j": {
+        name:"Iuri",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "ariel-j": {
+        name:"Ariel",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "lu-j": {
+        name:"Lu",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "levi-j": {
+        name:"Levi",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "leonardo-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "belo-j": {
+        name:"Belo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "ariele-j": {
+        name:"Ariele",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "Juliana-j": {
+        name:"Juliana",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "leonardo-police-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "aderbal-j": {
+        name:"Aderbal",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "leonardo-JESUS-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "thayane-j": {
+        name:"Thayane",
+        familyType: type.friendM,
+        bingo: [],
+    },
 }
--- src/data/testttt.js
+++ src/data/testttt.js
@@ -11,376 +11,283 @@ const type = {
     grama: 'minha querida vó',
 };
 
-let joaolist ={
-    // "margarida": {
-    //     name:"Margarida",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "ines": {
-    //     name:"Inês",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "cecilia": {
-    //     name:"Cecília",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "veronica": {
-    //     name:"Verônica",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "nazare": {
-    //     name:"Nazare",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "miguel": {
-    //     name:"Míguel",
-    //     familyType: type.uncle,
-    //     bingo: [],
-    // },
-    // "sebastiao": {
-    //     name:"Sebastião",
-    //     familyType: type.uncle,
-    //     bingo: [],
-    // },
-    // "andeu": {
-    //     name:"Andeu",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "alex": {
-    //     name:"Alex",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "Magayve": {
-    //     name:"Magayve",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "Francisca": {
-    //     name:"Francisca",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "Mayane": {
-    //     name:"Mayane",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "Luiz": {
-    //     name:"Luiz",
-    //     familyType: type.uncle,
-    //     bingo: [],
-    // },
-    // "Andressa": {
-    //     name:"Andressa",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "igor": {
-    //     name:"Igor",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "paulo": {
-    //     name:"Paulo",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "raquel": {
-    //     name:"Raquel",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "marcelo": {
-    //     name:"Marcelo",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "henrique": {
-    //     name:"Henrique",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "natalia": {
-    //     name:"Natalia",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "aline": {
-    //     name:"Aline",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "alice": {
-    //     name:"Alice",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "alane": {
-    //     name:"Alane",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "janine": {
-    //     name:"Janine",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "janiele": {
-    //     name:"Janiele",
-    //     familyType: type.cousinF,
-    //     bingo: [],
-    // },
-    // "geraldo": {
-    //     name:"Geraldo",
-    //     familyType: type.uncle,
-    //     bingo: [],
-    // },
-    // "david": {
-    //     name:"David",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "harley": {
-    //     name:"Harley",
-    //     familyType: type.cousinM,
-    //     bingo: [],
-    // },
-    // "maria-para": {
-    //     name:"Maria",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "neta": {
-    //     name:"Neta",
-    //     familyType: type.aunt,
-    //     bingo: [],
-    // },
-    // "donaSocorro": {
-    //     name:"Dona Socorro",
+let joaolist = {
+    "anderson-j": {
+        name:"Anderson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "josivan-j": {
+        name:"Josivan",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jackson-j": {
+        name:"Jackson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jeferson-j": {
+        name:"Jeferson",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "dhonas-j": {
+        name:"Dhonas",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "marcelo-j": {
+        name:"Marcelo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "eric-j": {
+        name:"Eric",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "bruno-j": {
+        name:"Bruno",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "davi-j": {
+        name:"Davi",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "nelio-j": {
+        name:"Nelio",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "wesley-j": {
+        name:"Wesley",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jayara-j": {
+        name:"Jayara",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "mike-j": {
+        name:"Mike",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "carmen-j": {
+        name:"Carmen",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "ricardo-j": {
+        name:"Ricardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "fabio-j": {
+        name:"Fabio",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "junior-leo-j": {
+        name:"Junior",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "pedro-j": {
+        name:"Pedro",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "matheus-j": {
+        name:"Matheus",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "jaine-j": {
+        name:"Jaine",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "igor-j": {
+        name:"Igor",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "vinicius-j": {
+        name:"Vinicius",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "iuri-j": {
+        name:"Iuri",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "ariel-j": {
+        name:"Ariel",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "lu-j": {
+        name:"Lu",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "levi-j": {
+        name:"Levi",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "leonardo-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "belo-j": {
+        name:"Belo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "ariele-j": {
+        name:"Ariele",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "Juliana-j": {
+        name:"Juliana",
+        familyType: type.friendF,
+        bingo: [],
+    },
+    "leonardo-police-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "aderbal-j": {
+        name:"Aderbal",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "leonardo-JESUS-j": {
+        name:"Leonardo",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    "thayane-j": {
+        name:"Thayane",
+        familyType: type.friendM,
+        bingo: [],
+    },
+    // "Camila-m": {
+    //     name:"Camila",
     //     familyType: type.friendF,
     //     bingo: [],
     // },
-    // "elaine": {
-    //     name:"Elaine",
+    // "Brendon-m": {
+    //     name:"Brendon",
+    //     familyType: type.friendM,
+    //     bingo: [],
+    // },
+    // "shirley-m": {
+    //     name:"Shirley",
     //     familyType: type.friendF,
     //     bingo: [],
     // },
-    // "valdo": {
-    //     name:"Valdo",
+    // "isabel-m": {
+    //     name:"Isabel",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "cassio-m": {
+    //     name:"Cassio",
     //     familyType: type.friendM,
     //     bingo: [],
     // },
-    // "profjoao": {
-    //     name:"Prof. João",
+    // "mateus-m": {
+    //     name:"Mateus",
     //     familyType: type.friendM,
     //     bingo: [],
     // },
-    // "maeMaria": {
-    //     name:"Maria",
-    //     familyType: type.mother,
+    // "junior-m": {
+    //     name:"Junior",
+    //     familyType: type.friendM,
     //     bingo: [],
     // },
-    // "zezinho": {
-    //     name:"Zezinho",
-    //     familyType: type.father,
+    // "neto-m": {
+    //     name:"Neto",
+    //     familyType: type.friendM,
     //     bingo: [],
     // },
-    // "janivan": {
-    //     name:"Janivan",
-    //     familyType: type.cousinM,
+    // "chris-m": {
+    //     name:"Chris",
+    //     familyType: type.friendF,
     //     bingo: [],
     // },
-
-
-
-    "janaina": {
-        name:"Janaína",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "rosinha": {
-        name:"rosinha",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "jairo": {
-        name:"Jairo",
-        familyType: type.cousinM,
-        bingo: [],
-    },
-    "hosana": {
-        name:"Hosana",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "carol": {
-        name:"Carol",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "gerson": {
-        name:"Gerson",
-        familyType: type.cousinM,
-        bingo: [],
-    },
-    "sinelandia": {
-        name:"Sinelandia",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "joaoPaulo": {
-        name:"João Paulo",
-        familyType: type.cousinM,
-        bingo: [],
-    },
-    "nivea": {
-        name:"Nivea",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "junior": {
-        name:"Júnior",
-        familyType: type.cousinM,
-        bingo: [],
-    },
-    "sonia": {
-        name:"Sonia",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "camila": {
-        name:"Camila",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "rosa": {
-        name:"Rosa",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "antoniaMaria": {
-        name:"Antonia Maria",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "daniele": {
-        name:"Daniele",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "leda": {
-        name:"Leda",
-        familyType: type.grama,
-        bingo: [],
-    },
-    "toinha": {
-        name:"Toinha",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "kelma": {
-        name:"Kelma",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "goreti": {
-        name:"Goreti",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "Edmea": {
-        name:"Edmea",
-        familyType: type.grama,
-        bingo: [],
-    },
-    "paula": {
-        name:"Paula",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "monica": {
-        name:"Monica",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "fablicia": {
-        name:"Fablicia",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "fabiana": {
-        name:"Fabiana",
-        familyType: type.cousinF,
-        bingo: [],
-    },
-    "vivian": {
-        name:"Vivian",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "juliara": {
-        name:"Juliara",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "lucia": {
-        name:"Lucia",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "Zelhinha": {
-        name:"Zelhinha",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "mase": {
-        name:"Mase",
-        familyType: type.aunt,
-        bingo: [],
-    },
-    "moesio": {
-        name:"Moesio",
-        familyType: type.uncle,
-        bingo: [],
-    },
-    "catia": {
-        name:"Cátia",
-        familyType: type.mother,
-        bingo: [],
-    },
-    "marcos": {
-        name:"Marcos",
-        familyType: type.father,
-        bingo: [],
-    },
-    "patricia": {
-        name:"Patrícia",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "pamela": {
-        name:"Pamela",
-        familyType: type.friendF,
-        bingo: [],
-    },
-    "antonia": {
-        name:"Antonia",
-        familyType: type.aunt,
-        bingo: [],
-    },
+    // "kamila-m": {
+    //     name:"Kamila",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "orle-m": {
+    //     name:"Orle",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "yago-m": {
+    //     name:"Yago",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "amanda-m": {
+    //     name:"Amanda",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "rylderson-m": {
+    //     name:"Rylderson",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "luiza-m": {
+    //     name:"Luiza",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "dayane-m": {
+    //     name:"Dayane",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "nadia-m": {
+    //     name:"Nadia",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "ellen-m": {
+    //     name:"Ellen",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "milleny-m": {
+    //     name:"Milleny",
+    //     familyType: type.friendF,
+    //     bingo: [],
+    // },
+    // "caina-m": {
+    //     name:"Caina",
+    //     familyType: type.friendM,
+    //     bingo: [],
+    // },
 }
 
 Object.keys(joaolist).map(el => {
     const {name, familyType} = joaolist[el]
     console.log(
-        `Olá ${name}, ${familyType} este é o link para participar do nosso chá de casa nova online, estamos ansiosos com a sua presença. Não esqueça, será dia 29 de maio as 15 horas. Venha comemorar nossa conquista com a gente. Link da videochamada: https://meet.google.com/pog-kwwu-new
+        `Olá ${name}, ${familyType} este é o link para participar do nosso chá de casa nova online, estamos ansiosos com a sua presença. Não esqueça, será amanhã, dia 30 de maio as 15 horas. Venha comemorar nossa conquista com a gente. Link da videochamada: https://meet.google.com/jmg-rryg-zeg
 
 *Como foi prometido teremos um bingo valendo um jantar.* E este é o link da sua cartela de bingo. Boa sorteee!!! https://joaowillamy.github.io/family-bingo/#/?code=${el}
 
